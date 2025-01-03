4371c69e5cea4d7b64d054f4414717db3ae73e8d
moon <th@droomo.com>
2025-01-03 17:38:47 +0800

    feat: 添加Athena Emit State节点

---
+++ api/core/tools/provider/builtin/athena/tools/emit_state.py
@@ -0,0 +1,26 @@
+from typing import Any
+from core.tools.tool.builtin_tool import BuiltinTool
+
+
+class EmitState(BuiltinTool):
+    """
+    Emit state on Athena chat frontend.
+    First 6 characters of the message belong to protocal header.
+    """
+    def _invoke(
+        self, user_id: str, tool_parameters: dict[str, Any]
+    ):
+        main_state = tool_parameters.get("main_state")
+        sub_state = tool_parameters.get("sub_state")
+
+        response = []
+        if main_state:
+            text_message = self.create_text_message(f"emstat:main:{main_state}")
+            response.append(text_message)
+            yield text_message
+        if sub_state:
+            text_message = self.create_text_message(f"emstat:sub:{sub_state}")
+            response.append(text_message)
+            yield text_message
+
+        return response
