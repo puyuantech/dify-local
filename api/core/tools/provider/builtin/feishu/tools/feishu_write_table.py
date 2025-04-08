966bf89bbf2d7049d104f34fdc97895f106fbde4
whxway <whxway@whu.edu.cn>
2025-04-08 17:39:13 +0800

    update slark

--- api/core/tools/provider/builtin/feishu/tools/feishu_write_table.py
+++ api/core/tools/provider/builtin/feishu/tools/feishu_write_table.py
@@ -1,8 +1,7 @@
 from typing import Any, Union
 
-import anyio
 import pandas as pd
-from slark import AsyncLark
+from slark import Lark
 
 from core.tools.entities.tool_entities import ToolInvokeMessage
 from core.tools.tool.builtin_tool import BuiltinTool
@@ -21,16 +20,11 @@ def extract_table(md_text):
                 continue
             table_elements.append(cells)
     assert table_elements, "No tables found in the Markdown text."
-    assert (
-        len(table_elements) > 1
-    ), "At least two table lines must be present in the Markdown text."
+    assert len(table_elements) > 1, "At least two table lines must be present in the Markdown text."
     for cells in table_elements[1:]:
-        assert len(cells) == len(
-            table_elements[0]
-        ), "All tables must have the same number of columns."
+        assert len(cells) == len(table_elements[0]), "All tables must have the same number of columns."
     df = pd.DataFrame(table_elements[1:], columns=table_elements[0])
     return df
-
 
 
 class FeishuWriteTableTool(BuiltinTool):
@@ -43,15 +37,12 @@ class FeishuWriteTableTool(BuiltinTool):
         table_data = tool_parameters.get("table_data", "")
         if not table_data:
             return self.create_text_message("Invalid parameter table_data")
-        
+
         credentials = self.runtime.credentials
         try:
             df = extract_table(table_data)
-            lark = AsyncLark(app_id=credentials["app_id"], app_secret=credentials["app_secret"])
-            async def write_feishu_table(table_url, df, has_header=True):
-                return await lark.sheets.write(table_url, data=df, has_header=has_header)
-            
-            response = anyio.run(write_feishu_table, table_url, df)
-            return self.create_text_message(str(response))
+            lark = Lark(app_id=credentials["app_id"], app_secret=credentials["app_secret"])
+            resp = lark.sheets.write(table_url, data=df, has_header=True)
+            return self.create_text_message(str(resp))
         except Exception as e:
             return self.create_text_message("Failed to get table. {}".format(e))
